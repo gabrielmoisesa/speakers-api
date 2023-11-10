e358bc67c2c4b7b0c6fdc9721ecc5213cbe3448f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-10 17:37:48 -0300

    req 3: create generate token

---
+++ src/generateToken.js
@@ -0,0 +1,14 @@
+const generateToken = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const tokenLength = 16;
+  let token = '';
+
+  for (let i = 0; i < tokenLength; i += 1) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters.charAt(randomIndex);
+  }
+
+  return token;
+};
+
+module.exports = generateToken;
