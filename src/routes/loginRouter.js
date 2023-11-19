4c07276a3ae1c66abd153eb1feaf1c9a93b881b6
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-19 00:17:25 -0300

    fix: login router post path

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -5,7 +5,7 @@ const { generateToken } = require('../utils');
 
 const router = express.Router();
 
-router.post('/login', validateLogin, (_req, res) => {
+router.post('/', validateLogin, (_req, res) => {
   res.status(200).json({ token: generateToken() });
 });
 
