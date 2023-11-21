87ec56cab8fec3f6f028dae62cf252af94252494
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-21 03:14:16 -0300

    refactor: remove unecessary get & change listen log

--- src/server.js
+++ src/server.js
@@ -3,12 +3,8 @@ const connection = require('./db/connection');
 
 const PORT = process.env.PORT || '3001';
 
-app.get('/', (_request, response) => {
-  response.status(200).send();
-});
-
 app.listen(PORT, async () => {
-  console.log('Online');
+  console.log(`Online on port ${PORT}`);
 
   const [result] = await connection.execute('SELECT 1');
   if (result) console.log('MySQL connection OK');
