c53cb1931a9c9d8aadaec3330ce4a9efd37858f7
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-27 17:33:08 -0300

    refactor: move app error middleware to error handler and use

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const { talkerRouter, loginRouter } = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use('/talker', talkerRouter);
 app.use('/login', loginRouter);
-
-app.use((err, _req, res, _next) => {
-  res.status(500).json({ message: `error: ${err.message}` });
-});
+app.use(errorHandler);
 
 module.exports = app;
